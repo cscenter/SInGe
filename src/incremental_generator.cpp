ae4f8f4d27a3fc1471b2812e1b06149b1c9e2c61
Vladimir Skipor <vladimirskipor@gmail.com>
2014-10-29 01:01:01 +0300

    cmake edit

---
+++ src/incremental_generator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <google/vcencoder.h>
+#include <google/vcdecoder.h>
+#include <dictionary.hpp>
+#include <statistics.h>
+#include <boost/iostreams/device/mapped_file.hpp>
+
+using std::string;
+using open_vcdiff::Statistics;
+using std::cout;
+using std::endl;
+using namespace boost::iostreams;
+
+
+int main() {
+  //code coming soon...
+
+  std::cout << "not implemented yet :(" << std::endl;
+
+  return 0;
+}
