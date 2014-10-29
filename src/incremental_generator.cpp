2077c79423569341a0845afd9ddfa24ce9628667
Vladimir Skipor <vladimirskipor@gmail.com>
2014-10-29 19:17:55 +0300

    Delete BOOST linking, unused makefile

--- src/incremental_generator.cpp
+++ src/incremental_generator.cpp
@@ -3,13 +3,11 @@
 #include <google/vcdecoder.h>
 #include <dictionary.hpp>
 #include <statistics.h>
-#include <boost/iostreams/device/mapped_file.hpp>
 
 using std::string;
 using open_vcdiff::Statistics;
 using std::cout;
 using std::endl;
-using namespace boost::iostreams;
 
 
 int main() {
